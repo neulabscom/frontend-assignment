2d43c603a53f53384df6e20e4a7704aa29a13e04
Maio93 <maio.cocchi@hotmail.it>
2021-12-06 20:50:34 +0000

    complete discovery button

---
+++ src/stories/DiscoveryButton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import DiscoveryButtonImg from './assets/discovery-button.svg'
+import DiscoveryButtonSelectedImg from './assets/discovery-button-selected.svg'
+import './DiscoveryButton.css'
+
+interface DiscoveryButtonProps {
+    selected?: boolean
+    onClick: () => void
+}
+
+export const DiscoveryButton = ({
+    onClick,
+    selected = false,
+}: DiscoveryButtonProps) => {
+    const containerClass = selected ? 'disc-btn-container-selected' : 'disc-btn-container'
+    return (
+        <div className={containerClass} >
+            <img className='default' src={DiscoveryButtonImg} />
+            <img className='selected' src={DiscoveryButtonSelectedImg} />
+        </div>
+    )
+}
